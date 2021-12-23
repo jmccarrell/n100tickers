46c3b26e52fa74b3c52ed7b4936dbae3acc74fd2
Jeff McCarrell <jeff@mccarrell.org>
2021-12-22 18:24:55 -0800

    2021 end of year changes; initial 2022 support.

--- docs/index.rst
+++ docs/index.rst
@@ -21,8 +21,8 @@ set of ticker symbols (eg, ``AAPL``) that were in the index on that date.
 Coverage
 --------
 
-As of version |release|, accurate coverage is provided from Jan 1, 2016 through at least October 1, 2021.  Most
-likely, the coverage is accurate further into 2021 subject to additional changes being announced by Nasdaq.  A new
+As of version |release|, accurate coverage is provided from Jan 1, 2016 through at least December 20, 2021.  Most
+likely, the coverage is accurate further into 2022 subject to additional changes being announced by Nasdaq.  A new
 version of the API is released on each update Nasdaq announces, typically with a time lag of a few days to a
 few weeks.  It is the intent of the project maintainers to provide accurate coverage on an ongoing basis.
 
--- tests/test_n100_tickers.py
+++ tests/test_n100_tickers.py
@@ -28,7 +28,7 @@ def _test_one_swap(as_of_date: datetime.date,
     assert added_ticker in tickers_on_change_date
 
 
-def _test_at_year_boundary(year: int, expected_number_of_tickers: int) -> None:
+def _test_at_year_boundary(year: int) -> None:
     """prove the tickers at the beginning of the year match the set at the end of the
     previous year.
     """
@@ -44,20 +44,32 @@ def _test_at_year_boundary(year: int, expected_number_of_tickers: int) -> None:
     assert previous_tickers == current_tickers
 
 
-# def test_year_boundary_2021_2022() -> None:
-#     _test_at_year_boundary(2022, 102)
+def test_year_boundary_2021_2022() -> None:
+    _test_at_year_boundary(2022)
 
 
-# def test_2021_annual_changes() -> None:
-#     num_tickers_2021_end_of_year = 102
-#     # Annual 2021 changes
-#     # https://www.nasdaq.com/press-release/annual-changes-to-the-nasdaq-100-indexr-2021-12-10-0
-#     #
-#     # On December 10, 2021 Nasdaq announced that six new companies would join the index prior to the market open on December 20, 2021.
-#     # They are Airbnb (ABNB), Datadog (DDOG), Fortinet (FTNT), Lucid Group (LCID), Palo Alto Networks (PANW), and Zscaler (ZS).
-#     # They will replace CDW (CDW), Cerner (CERN), Check Point (CHKP), Fox Corporation (FOXA/FOX), Incyte (INCY), and Trip.com (TCOM).
-#     # https://greenstocknews.com/news/nasdaq/lcid/annual-changes-to-the-nasdaq-100-index
-#     assert len(tickers_as_of(2021, 12, 17)) == num_tickers_2021_end_of_year
+def test_2021_annual_changes() -> None:
+    num_tickers_2021_end_of_year = 101
+    # Annual 2021 changes
+    # https://www.nasdaq.com/press-release/annual-changes-to-the-nasdaq-100-indexr-2021-12-10-0
+    #
+    # On December 10, 2021 Nasdaq announced that six new companies would join the index
+    #  prior to the market open on December 20, 2021.
+    # They are Airbnb (ABNB), Datadog (DDOG), Fortinet (FTNT), Lucid Group (LCID),
+    #   Palo Alto Networks (PANW), and Zscaler (ZS).
+    # They will replace CDW (CDW), Cerner (CERN), Check Point (CHKP), Fox Corporation (FOXA/FOX),
+    #  Incyte (INCY), and Trip.com (TCOM).
+    # https://greenstocknews.com/news/nasdaq/lcid/annual-changes-to-the-nasdaq-100-index
+    # This removes 7 tickers while adding 6, so total number of tickers goes to 101
+    assert len(tickers_as_of(2021, 12, 17)) == num_tickers_2021_end_of_year + 1
+    tickers_removed_2021_12_20 = frozenset(('CDW', 'CERN', 'CHKP', 'FOX', 'FOXA', 'INCY', 'TCOM'))
+    assert tickers_removed_2021_12_20.issubset(tickers_as_of(2021, 12, 17))
+    tickers_added_2021_12_20 = frozenset(('ABNB', 'DDOG', 'FTNT', 'LCID', 'PANW', 'ZS'))
+    assert tickers_added_2021_12_20.isdisjoint(tickers_as_of(2021, 12, 17))
+
+    assert len(tickers_as_of(2021, 12, 20)) == num_tickers_2021_end_of_year
+    assert tickers_removed_2021_12_20.isdisjoint(tickers_as_of(2021, 12, 20))
+    assert tickers_added_2021_12_20.issubset(tickers_as_of(2021, 12, 20))
 
 
 def test_tickers_2021() -> None:
@@ -71,13 +83,13 @@ def test_tickers_2021() -> None:
 
 
 def test_year_boundary_2020_2021() -> None:
-    _test_at_year_boundary(2021, 102)
+    _test_at_year_boundary(2021)
 
 
 def test_tickers_2020() -> None:
     num_tickers_2020: int = 103
 
-    _test_at_year_boundary(2020, num_tickers_2020)
+    _test_at_year_boundary(2020)
 
     # On April 20, Dexcom replaced American Airlines Group in the index
     _test_one_swap(datetime.date.fromisoformat('2020-04-20'), 'AAL', 'DXCM', num_tickers_2020)
@@ -122,7 +134,7 @@ def test_2020_annual_changes() -> None:
 def test_tickers_2019() -> None:
     num_tickers_2019: int = 103
 
-    _test_at_year_boundary(2019, num_tickers_2019)
+    _test_at_year_boundary(2019)
 
     # 6 tickers added and removed on 12/23/2019
     # https://finance.yahoo.com/news/annual-changes-nasdaq-100-index-010510822.html
@@ -149,7 +161,7 @@ def test_tickers_2019() -> None:
 def test_tickers_2018() -> None:
     num_tickers_2018: int = 103
 
-    _test_at_year_boundary(2018, num_tickers_2018)
+    _test_at_year_boundary(2018)
 
     # 6 tickers added and removed on 12/24/2018
     # https://www.nasdaq.com/about/press-center/annual-changes-nasdaq-100-index-0
@@ -215,7 +227,7 @@ def test_tickers_2017() -> None:
 
 
 def test_year_boundary_2016_2017() -> None:
-    _test_at_year_boundary(2017, 104)
+    _test_at_year_boundary(2017)
 
 
 def test_2016_annual_changes() -> None:
