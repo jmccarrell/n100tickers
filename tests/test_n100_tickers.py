cd0b3500be2baa901f7c7d7b20f0e0902fd90820
Jeff McCarrell <jeff@mccarrell.org>
2023-12-23 20:35:51 -0800

    2023 end of year nasdaq 100 index updates. (#14)
    7 total companies switched out.

--- tests/test_n100_tickers.py
+++ tests/test_n100_tickers.py
@@ -44,6 +44,35 @@ def _test_at_year_boundary(year: int) -> None:
     assert previous_tickers == current_tickers
 
 
+def test_year_boundary_2023_2024() -> None:
+    _test_at_year_boundary(2024)
+
+
+def test_dec_2023_changes() -> None:
+    num_tickers_2023 = 101
+    assert len(tickers_as_of(2023, 1, 1)) == num_tickers_2023
+    assert len(tickers_as_of(2023, 10, 1)) == num_tickers_2023
+
+    # On Dec 14, SeaGen (SGEN) was dropped after its merger with Pfizer, it was replaced by Take-Two
+    # Interactive (TTWO).
+    _test_one_swap(datetime.date.fromisoformat('2023-12-14'), 'SGEN', 'TTWO', num_tickers_2023)
+
+    # On Dec 18, the annual re-ranking was applied:
+    # Align Technology Inc (ALGN), eBay (EBAY), Enphase Energy (ENPH), JD.com (JD), Lucid Group (LCID) and
+    # Zoom Video Communications (ZOOM) were dropped.
+    assert len(tickers_as_of(2023, 12, 15)) == num_tickers_2023
+    tickers_removed_2023_12_18 = frozenset(('ALGN', 'EBAY', 'ENPH', 'JD', 'LCID', 'ZM'))
+    assert tickers_removed_2023_12_18.issubset(tickers_as_of(2023, 12, 15))
+    assert tickers_removed_2023_12_18.isdisjoint(tickers_as_of(2023, 12, 18))
+
+    # CDW, Coca-Cola Europacific Partners (CCEP), DoorDash (DASH), MongoDB Inc (MDB), Roper Technologies
+    # (ROP) and Splunk (SPLK) were added.
+    tickers_added_2023_12_18 = frozenset(('CDW', 'CCEP', 'DASH', 'MDB', 'ROP', 'SPLK'))
+    assert tickers_added_2023_12_18.isdisjoint(tickers_as_of(2023, 12, 15))
+    assert tickers_added_2023_12_18.issubset(tickers_as_of(2023, 12, 18))
+    assert len(tickers_as_of(2023, 12, 18)) == num_tickers_2023
+
+
 def test_jun_jul_2023_changes() -> None:
     # On June 7, GE HealthCare Technologies replaced Fiserv.
     # On June 20, Onsemi replaced Rivian.
