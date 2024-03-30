749c3f9a6ed5c47e8a5b74e6bb17dde3e5c75a5b
Jeff McCarrell <jeff@mccarrell.org>
2024-03-30 12:33:05 -0700

    update importlib resource handling to 3.9 idiomatic usage.

--- nasdaq_100_ticker_history/n100tickers.py
+++ nasdaq_100_ticker_history/n100tickers.py
@@ -29,9 +29,9 @@ def _load_tickers_from_yaml(year: int = 2020) -> dict:
 
     module_name = "nasdaq_100_ticker_history"
     resource_name = f"n100-ticker-changes-{year}.yaml"
-    resource = importlib.resources.files(module_name).joinpath(resource_name)
+    resource: importlib.resources.abc.Traversable = importlib.resources.files(module_name).joinpath(resource_name)
 
-    if not resource.exists():
+    if not resource.is_file():
         raise NotImplementedError(
             f"no nasdaq 100 tickers defined for {year}; "
             f"cant find resource {resource_name} in package {module_name}"
